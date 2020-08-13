d293852232919e5e0a5a5ff4f282ff550e63793f
Emilin DADIE <dadie.emilin@gmail.com>
2020-08-14 01:00:00 +0200

    rename controller

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const AuthController = require('../controllers/main.controller');
+const MainController = require('../controllers/main.controller');
 
 /* GET data listing. */
 router.get('/', async function(req, res, next){
@@ -27,21 +27,21 @@ router.get('/', async function(req, res, next){
 });
 
 async function login(){
-   const loginResponse = await AuthController.login();
+   const loginResponse = await MainController.login();
    return loginResponse;
 }
 
 async function getAccessToken(refresh_token){
-   const accessTokenResponse = await AuthController.getAccessToken(refresh_token);
+   const accessTokenResponse = await MainController.getAccessToken(refresh_token);
    return accessTokenResponse;
 }
 
 async function recursiveGetAccounts(access_token){
    const accounts = [];
-   let userAccountsResponse = await AuthController.getRecursiveData(access_token, `${process.env.API_HOST}:${process.env.API_PORT}/accounts`);
+   let userAccountsResponse = await MainController.getRecursiveData(access_token, `${process.env.API_HOST}:${process.env.API_PORT}/accounts`);
    accounts.push(...userAccountsResponse.data.account);
    while(userAccountsResponse.data.link !== null && userAccountsResponse.data.link.next !== null){
-      userAccountsResponse = await AuthController.getRecursiveData(access_token, `${process.env.API_HOST}:${process.env.API_PORT}${userAccountsResponse.data.link.next}`);
+      userAccountsResponse = await MainController.getRecursiveData(access_token, `${process.env.API_HOST}:${process.env.API_PORT}${userAccountsResponse.data.link.next}`);
       accounts.push(...userAccountsResponse.data.account);
    }
    return accounts;
@@ -49,11 +49,11 @@ async function recursiveGetAccounts(access_token){
 
 async function recursiveGetTransactions(access_token, acc_number){
    const transactions = [];
-   let accountTransactionsResponse = await AuthController.getRecursiveData(access_token, `${process.env.API_HOST}:${process.env.API_PORT}/accounts/${acc_number}/transactions`);
+   let accountTransactionsResponse = await MainController.getRecursiveData(access_token, `${process.env.API_HOST}:${process.env.API_PORT}/accounts/${acc_number}/transactions`);
    
    transactions.push(...accountTransactionsResponse.data.transactions);
    while(accountTransactionsResponse.data.link !== null && accountTransactionsResponse.data.link.next !== null){
-      accountTransactionsResponse = await AuthController.getRecursiveData(access_token, `${process.env.API_HOST}:${process.env.API_PORT}${accountTransactionsResponse.data.link.next}`);
+      accountTransactionsResponse = await MainController.getRecursiveData(access_token, `${process.env.API_HOST}:${process.env.API_PORT}${accountTransactionsResponse.data.link.next}`);
       transactions.push(...accountTransactionsResponse.data.transactions);
    }
    return transactions;
